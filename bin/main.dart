ea217bc52599e5c72ca8a08ec2a610833e194dc4
steve <steve.hamblett@linux.com>
2015-10-20 10:02:06 +0100

    Full directory commit of css etc

---
+++ bin/main.dart
@@ -0,0 +1,16 @@
+/*
+ * Package : dartivity_control
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 12/10/2015
+ * Copyright :  S.Hamblett 2015
+ */
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dartivity_control/dartivity_control.dart' as dartivity;
+
+
+main(List<String> arguments) {
+
+}
---
+++ index.dart
@@ -0,0 +1,48 @@
+/*
+ * Package : dartivity_control
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 12/10/2015
+ * Copyright :  S.Hamblett 2015
+ */
+
+
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:dartivity_control/dartivity_control.dart';
+
+void main(List<String> arguments) {
+
+  // Get Apache
+  Apache myAp = new Apache();
+
+  // Get Dartivity Control
+  DartivityControl control = new DartivityControl(myAp);
+
+  // Pre-process the input parameters and despatch the request.
+  if ( myAp.Request.containsKey('id')) {
+
+    String id = myAp.Request['id'];
+    if ( id != null ) {
+
+      try {
+
+        int pageId = int.parse(id);
+        control.despatch(pageId);
+
+      } catch(e) {
+
+        control.despatch(DartivityControlCfg.defaultPage);
+      }
+    } else {
+
+      control.despatch(DartivityControlCfg.errorPage);
+    }
+  } else {
+    control.despatch(DartivityControlCfg.defaultPage);
+  }
+
+  // Flush buffers and exit
+  myAp.flushBuffers();
+	  
+}
